72f4d2d737fc0f8bca3d921607172061bd2e0368
Mark Mark <antiutka@gmail.com>
2022-04-12 15:46:55 +0300

    разбивка длинных строк

--- src/test/java/np/qa/lesson13/pages/MainPage.java
+++ src/test/java/np/qa/lesson13/pages/MainPage.java
@@ -15,8 +15,10 @@ public class MainPage {
             loginButton = $("#pt-login").$("a");
     public String ruWikiBase = "https://ru.wikipedia.org/wiki/",
             mainPageUrl = ruWikiBase +
-                    "%D0%9E%D0%B1%D1%81%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5:%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0",
+                    "%D0%9E%D0%B1%D1%81%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5" +
+                    ":%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0",
             mainPageDiscussUrl = ruWikiBase +
-                    "%D0%9E%D0%B1%D1%81%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5:%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0";
+                    "%D0%9E%D0%B1%D1%81%D1%83%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5" +
+                    ":%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0";
 
 }
